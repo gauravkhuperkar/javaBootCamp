a0b3e5e17dda12d6eda7974ee60379817e09bf8f
screwJack <gauravkh@thoughtworks.com>
2016-03-16 20:48:31 +0530

    [Jisnu/Gaurav] Added factory method inside Rectangle for validation of negative dimentions

--- ObjectBootCamp/src/Rectangle.java
+++ ObjectBootCamp/src/Rectangle.java
@@ -7,9 +7,16 @@ public class Rectangle {
     private double length;
     private double breadth;
 
-    public Rectangle(double length, double breadth) {
+    private Rectangle(double length, double breadth) {
         this.length = length;
         this.breadth = breadth;
+    }
+
+    public static Rectangle create(double length, double breadth){
+        if(length <= 0 || breadth <= 0)
+            throw new IllegalArgumentException();
+        return new Rectangle(length,breadth);
+
     }
 
     public double calculateArea(){
--- ObjectBootCamp/test/RectangleTest.java
+++ ObjectBootCamp/test/RectangleTest.java
@@ -1,22 +1,46 @@
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class RectangleTest {
+
+    @Test(expected= IllegalArgumentException.class)
+    public void calculateArea_should_throw_an_illegal_argument_exception_if_length_is_negative() {
+        double length = -4;
+        double breadth = 15.0;
+        Rectangle rectangle = Rectangle.create(length, breadth);
+    }
+
+    @Test(expected= IllegalArgumentException.class)
+    public void calculateArea_should_throw_an_illegal_argument_exception_if_breadth_is_negative() {
+        double length = 40;
+        double breadth = -15;
+        Rectangle rectangle = Rectangle.create(length, breadth);
+    }
+
     @Test
     public void calculateArea_should_return_area_of_rectangle_if_we_give_length_and_breadth_as_double() {
         double length = 10.0;
         double breadth = 15.0;
-        Rectangle rectangle = new Rectangle(length,breadth);
+        Rectangle rectangle = Rectangle.create(length,breadth);
         double expected = 150;
-        Assert.assertEquals(expected,rectangle.calculateArea(),0.001);
+        assertEquals(expected,rectangle.calculateArea(),0.001);
+    }
+
+
+    @Test(expected= IllegalArgumentException.class)
+    public void calculatePerimeter_should_throw_an_illegal_argument_exception_if_dimentions_are_negative() {
+        double length = -25;
+        double breadth = -62;
+        Rectangle rectangle = Rectangle.create(length, breadth);
     }
 
     @Test
     public void calculatePerimeter_should_return_perimeter_of_rectangle_if_we_give_length_and_breadth() {
         double length = 10;
         double breadth = 15;
-        Rectangle rectangle = new Rectangle(length,breadth);
+        Rectangle rectangle = Rectangle.create(length,breadth);
         double expected = 50;
-        Assert.assertEquals(expected,rectangle.calculatePerimeter(),0.001);
+        assertEquals(expected,rectangle.calculatePerimeter(),0.001);
     }
 }

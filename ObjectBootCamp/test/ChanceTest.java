1dd3bead429c2ead436c149ea08dfcf9de17396f
screwJack <gauravkh@thoughtworks.com>
2016-03-18 23:18:43 +0530

    modify chance tests of probability

--- ObjectBootCamp/test/ChanceTest.java
+++ ObjectBootCamp/test/ChanceTest.java
@@ -1,32 +1,42 @@
+import Exceptions.IllegalInputException;
 import chance.Chance;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.TestCase.assertEquals;
 
 public class ChanceTest {
     @Test
-    public void chanceOfGettingTailsShouldBe0Point5WhenOnlyOneCoinIsFlipped() throws Exception {
-        Chance chance = new Chance(2, 1);
-        assertEquals(0.5, chance.getProbabilityOfTail(), 0);
+    public void chanceShouldAbleToCalculateNotOfItself() throws IllegalInputException {
+        Chance chance = Chance.create(0.23);
+        assertEquals(Chance.create(0.77), chance.not());
     }
+
     @Test
-    public void chanceOfNotGettingTailsShouldBe0Point5WhenOnlyOneCoinIsFlipped() throws Exception {
-        Chance chance = new Chance(2, 1);
-        assertEquals(0.5, chance.getProbabilityOfNotTail(), 0);
+    public void chanceOfGettingTailsShouldBe0Point5WhenOnlyOneCoinIsFlipped() throws IllegalInputException {
+        Chance chance = Chance.create(0.5);
+//        Assert.assertEquals(chance.equals(chance));
+
     }
-    @Test
-    public void chanceOfGettingTailsShouldBe0Point25WhenTwoCoinsAreFlipped() throws Exception {
-        Chance chance = new Chance(2, 2);
-        assertEquals(0.25, chance.getProbabilityOfTail(), 0);
-    }
-    @Test
-    public void chanceOfNotGettingTailsShouldBe0Point75WhenTwoCoinsAreFlipped() throws Exception {
-        Chance chance = new Chance(2, 2);
-        assertEquals(0.75, chance.getProbabilityOfNotTail(), 0);
-    }
-    @Test
-    public void chanceOfGettingAtleastOneTailsShouldBe0Point75WhenTwoCoinsAreFlipped() throws Exception {
-        Chance chance = new Chance(2, 2);
-        assertEquals(0.75, chance.getProbabilityOfAtleastOneTail(), 0);
-    }
+
+    //    chanceOfGettingTailsShouldBe0Point5WhenOnlyOneCoinIsFlipped
+//    @Test
+//    public void chanceOfNotGettingTailsShouldBe0Point5WhenOnlyOneCoinIsFlipped() throws IllegalInputException {
+//        Chance chance = Chance.create(0.23);
+//        assertEquals(0.5, chance.getProbabilityOfNotTail(), 0);
+//    }
+//    @Test
+//    public void chanceOfGettingTailsShouldBe0Point25WhenTwoCoinsAreFlipped() throws IllegalInputException {
+//        Chance chance = Chance.create(0.23);
+//        assertEquals(0.25, chance.getProbabilityOfTail(), 0);
+//    }
+//    @Test
+//    public void chanceOfNotGettingTailsShouldBe0Point75WhenTwoCoinsAreFlipped() throws IllegalInputException {
+//        Chance chance = Chance.create(0.23);
+//        assertEquals(0.75, chance.getProbabilityOfNotTail(), 0);
+//    }
+//    @Test
+//    public void chanceOfGettingAtleastOneTailsShouldBe0Point75WhenTwoCoinsAreFlipped() throws IllegalInputException {
+//        Chance chance = Chance.create(0.23);
+//        assertEquals(0.75, chance.getProbabilityOfAtleastOneTail(), 0);
+//    }
 }
